55d71718ffaea8399caf79bef0fecf20049cd275
thanhnd <nguyenduythanh1623@gmail.com>
2025-08-21 10:39:03 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,157 @@
+import { useCallback, useEffect, useState } from "react";
+import { useWeb3 } from "./dist";
+
+function App() {
+  const [email, setEmail] = useState("");
+  const [step, setStep] = useState<"email" | "otp">("email");
+  const [otp, setOTP] = useState("");
+  // const [OTPCount, setOTPCount] = useState(0);
+  const [error, setError] = useState<string | null>(null);
+
+  const {
+    loginMagic,
+    verifyOTPMagic,
+    // setIsSendingOTP,
+    checkLoggedInMagic,
+    disconnectWallet,
+  } = useWeb3();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("Đang đăng nhập với:", { email });
+
+    await loginMagic?.({
+      email: email,
+      onSuccess: () => {
+        setStep("otp");
+      },
+      onFail: () => {},
+      onOTPSent: () => setStep("otp"),
+      onVerifyOTPFail: () => {
+        setOTP("");
+        setError("Invalid OTP");
+      },
+      onLocked: () => {
+        console.log("Locked");
+        setStep("email");
+        setOTP("");
+        setError("Locked: Too many attempts");
+      },
+      onExpiredEmailOTP: () => {
+        setError("OTP Expired");
+      },
+      onLoginThrottled: () => {
+        setError("Login Throttled");
+      },
+      onDone: () => {
+        setError(null);
+        console.log("Đăng nhập thanh cong", step);
+      },
+      onError: (reason) => {
+        setError("Login Failed");
+        console.log(">>>>>> reason", reason);
+        console.log(">>>>>> reason.code", reason?.code);
+        console.log(">>>>>> reason.message", reason?.message);
+        {
+          /****
+          >>>>>> reason r5: Magic RPC Error: [-32602] Invalid params: Please provide a valid email address. Dia chi email khong hop le
+          >>>>>> reason r5: Magic RPC Error: [-32603] Internal server error : Loi nay se roi vao cac truong hop: nhap sai otp 3 lan lien tiep, khong co mang khi login
+           */
+        }
+        if (reason?.code === -32603) {
+          setStep("email");
+        }
+      },
+    });
+  };
+
+  const handleLogout = useCallback(
+    async (e: React.FormEvent) => {
+      console.log({ disconnectWallet });
+      e.preventDefault();
+      await disconnectWallet();
+    },
+    [disconnectWallet]
+  );
+
+  useEffect(() => {
+    if (step !== "otp") return;
+
+    if (otp.length === 6) {
+      verifyOTPMagic?.(otp, () => {});
+    }
+  }, [otp, step]);
+
+  return (
+    <>
+      <div>
+        <form onSubmit={handleLogout}>
+          <button
+            type="submit"
+            className="w-full bg-blue-600 hover:bg-blue-700 text-white p-2 rounded-lg transition"
+          >
+            Logout
+          </button>
+        </form>
+        <button
+          onClick={async () => {
+            const rs = await checkLoggedInMagic();
+            console.log({ rs });
+          }}
+        >
+          Check
+        </button>
+      </div>
+      {step === "email" ? (
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+          <form
+            onSubmit={handleSubmit}
+            className="bg-white p-6 rounded-xl shadow-md w-full max-w-sm"
+          >
+            <h2 className="text-2xl font-bold mb-4 text-center">Đăng nhập</h2>
+
+            <div className="mb-4">
+              <label className="block text-sm font-medium mb-1">Email</label>
+              <input
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="w-full border border-gray-300 p-2 rounded-lg focus:ring-2 focus:ring-blue-500 outline-none"
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="w-full bg-blue-600 hover:bg-blue-700 text-white p-2 rounded-lg transition"
+            >
+              Đăng nhập
+            </button>
+          </form>
+        </div>
+      ) : (
+        <div>
+          <form
+            onSubmit={handleLogout}
+            className="bg-white p-6 rounded-xl shadow-md w-full max-w-sm"
+          >
+            <h2 className="text-2xl font-bold mb-4 text-center">OTP</h2>
+
+            <div className="mb-4">
+              <label className="block text-sm font-medium mb-1">
+                INPUT OTP
+              </label>
+              <input
+                value={otp}
+                onChange={(e) => setOTP(e.target.value)}
+                className="w-full border border-gray-300 p-2 rounded-lg focus:ring-2 focus:ring-blue-500 outline-none"
+              />
+            </div>
+
+            {error && <p className="text-red-500">{error}</p>}
+          </form>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default App;
